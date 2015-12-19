67cbefca1d48a40455c354405265944932231791
Ranieri Althoff <ranisalt@gmail.com>
2015-12-19 10:46:34 -0200

    Make long salt callback really async

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var bindings = require("bindings")("argon2_lib");
 exports.encrypt = function (plain, salt, callback) {
   "use strict";
 
-  if (salt.length < 16) {
-    callback(new Error("Salt too long, maximum 16 characters."), null);
+  if (salt.length > 16) {
+    process.nextTick(function () {
+      callback(new Error("Salt too long, maximum 16 characters."), null);
+    });
     return;
   }
 
