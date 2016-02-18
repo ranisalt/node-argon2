1b68b249190751f9a20bb6efb0831cc788233483
Ranieri Althoff <ranisalt@gmail.com>
2016-02-18 13:29:24 -0200

    Fix benchmarks to use promises and run them after tests

--- benchmark.js
+++ benchmark.js
@@ -2,13 +2,14 @@ const argon2 = require('./');
 const async = require('async');
 const benchmark = require('async-benchmark');
 
+console.warn = () => {};
 const password = 'password';
 const salt = argon2.generateSaltSync();
 const hash = argon2.hashSync(password, salt);
 
 const fixtures = [{
   name: 'argon2#hash',
-  func: (done) => argon2.hash(password, salt, done)
+  func: (done) => argon2.hash(password, salt).then(done)
 }, {
   name: 'argon2#hashSync',
   func: (done) => { argon2.hashSync(password, salt); done(); }
@@ -16,31 +17,31 @@ const fixtures = [{
   name: 'argon2#hashTimeCost',
   func: (done) => { argon2.hash(password, salt, {
       timeCost: argon2.defaults.timeCost + 3
-    }, done); }
+    }).then(done); }
 }, {
   name: 'argon2#hashMemoryCost',
   func: (done) => { argon2.hash(password, salt, {
       memoryCost: argon2.defaults.memoryCost + 3
-    }, done); }
+    }).then(done); }
 }, {
   name: 'argon2#hashParallelism',
   func: (done) => { argon2.hash(password, salt, {
       parallelism: argon2.defaults.parallelism + 3
-    }, done); }
+    }).then(done); }
 }, {
   name: 'argon2#hashArgon2d',
   func: (done) => { argon2.hash(password, salt, {
       argon2d: true
-    }, done); }
+    }).then(done); }
 }, {
   name: 'argon2#verify',
-  func: (done) => argon2.verify(hash, password, done)
+  func: (done) => argon2.verify(hash, password).then(done)
 }, {
   name: 'argon2#verifySync',
   func: (done) => { argon2.verifySync(hash, password); done(); }
 }, {
   name: 'argon2#generateSalt',
-  func: (done) => argon2.generateSalt(done)
+  func: (done) => argon2.generateSalt().then(done)
 }, {
   name: 'argon2#generateSaltSync',
   func: (done) => { argon2.generateSaltSync(); done(); }
