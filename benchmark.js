ce8ac6a3eb7da916855a067c588c3a45529a6558
Ranieri Althoff <ranisalt@gmail.com>
2016-05-25 21:32:22 -0300

    Use async/await on benchmark to drop dependencies

--- benchmark.js
+++ benchmark.js
@@ -1,76 +1,59 @@
-const async = require('async')
-const benchmark = require('async-benchmark')
-const argon2 = require('./')
+import benchmark from 'benchmark'
+import argon2 from './'
 
-console.warn = () => {}
-const password = 'password'
-const salt = argon2.generateSaltSync()
-const hash = argon2.hashSync(password, salt)
+(async () => {
+  console.log('Starting benchmark')
+  const password = 'password'
+  const salt = await argon2.generateSalt()
+  const hash = await argon2.hash(password, salt)
 
-const fixtures = [{
-  name: 'argon2#hash',
-  func: done => argon2.hash(password, salt).then(done)
-}, {
-  name: 'argon2#hashSync',
-  func: done => {
-    argon2.hashSync(password, salt)
-    done()
-  }
-}, {
-  name: 'argon2#hashTimeCost',
-  func: done => {
-    argon2.hash(password, salt, {
-      timeCost: argon2.defaults.timeCost + 3
-    }).then(done)
-  }
-}, {
-  name: 'argon2#hashMemoryCost',
-  func: done => {
-    argon2.hash(password, salt, {
-      memoryCost: argon2.defaults.memoryCost + 3
-    }).then(done)
-  }
-}, {
-  name: 'argon2#hashParallelism',
-  func: done => {
-    argon2.hash(password, salt, {
-      parallelism: argon2.defaults.parallelism + 3
-    }).then(done)
-  }
-}, {
-  name: 'argon2#hashArgon2d',
-  func: done => {
-    argon2.hash(password, salt, {
-      argon2d: true
-    }).then(done)
-  }
-}, {
-  name: 'argon2#verify',
-  func: done => argon2.verify(hash, password).then(done)
-}, {
-  name: 'argon2#verifySync',
-  func: done => {
-    argon2.verifySync(hash, password)
-    done()
-  }
-}, {
-  name: 'argon2#generateSalt',
-  func: done => argon2.generateSalt().then(done)
-}, {
-  name: 'argon2#generateSaltSync',
-  func: done => {
-    argon2.generateSaltSync()
-    done()
-  }
-}]
+  const fixtures = [{
+    name: 'argon2#hash',
+    func: async () => {
+      await argon2.hash(password, salt)
+    }
+  }, {
+    name: 'argon2#hashTimeCost',
+    func: async () => {
+      await argon2.hash(password, salt, {
+        timeCost: argon2.defaults.timeCost + 3
+      })
+    }
+  }, {
+    name: 'argon2#hashMemoryCost',
+    func: async () => {
+      await argon2.hash(password, salt, {
+        memoryCost: argon2.defaults.memoryCost + 3
+      })
+    }
+  }, {
+    name: 'argon2#hashParallelism',
+    func: async () => {
+      await argon2.hash(password, salt, {
+        parallelism: argon2.defaults.parallelism + 3
+      })
+    }
+  }, {
+    name: 'argon2#hashArgon2d',
+    func: async () => {
+      await argon2.hash(password, salt, {
+        argon2d: true
+      })
+    }
+  }, {
+    name: 'argon2#verify',
+    func: async () => await argon2.verify(hash, password)
+  }, {
+    name: 'argon2#generateSalt',
+    func: async () => await argon2.generateSalt()
+  }]
 
-async.eachSeries(fixtures, (item, callback) => {
-  benchmark(item.name, done => item.func(done), (err, ev) => {
-    console.log(ev.target.toString())
-    callback(err)
-  })
-}, err => {
-  if (err) {
-    console.dir(err)
+  for (const item of fixtures) {
+    benchmark(item.name, async () => {
+      await item.func()
+    }, (err, ev) => {
+      console.log(ev.target.toString())
+      console.log(err)
+    })
   }
-})
+})()
