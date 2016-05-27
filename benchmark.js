b0257e2d989be8a83df5cebba65c78377cb55465
Ranieri Althoff <ranisalt@gmail.com>
2016-05-27 19:24:51 -0300

    Simplify syntax for benchmark

--- benchmark.js
+++ benchmark.js
@@ -1,59 +1,46 @@
-import benchmark from 'benchmark'
+import {Suite} from 'benchmark'
 import argon2 from './'
 
 (async () => {
-  console.log('Starting benchmark')
   const password = 'password'
   const salt = await argon2.generateSalt()
   const hash = await argon2.hash(password, salt)
 
-  const fixtures = [{
-    name: 'argon2#hash',
-    func: async () => {
+  const fixtures = {
+    'argon2#hash': async () => {
       await argon2.hash(password, salt)
-    }
-  }, {
-    name: 'argon2#hashTimeCost',
-    func: async () => {
+    },
+    'argon2#hashTimeCost': async () => {
       await argon2.hash(password, salt, {
         timeCost: argon2.defaults.timeCost + 3
       })
-    }
-  }, {
-    name: 'argon2#hashMemoryCost',
-    func: async () => {
+    },
+    'argon2#hashMemoryCost': async () => {
       await argon2.hash(password, salt, {
         memoryCost: argon2.defaults.memoryCost + 3
       })
-    }
-  }, {
-    name: 'argon2#hashParallelism',
-    func: async () => {
+    },
+    'argon2#hashParallelism': async () => {
       await argon2.hash(password, salt, {
         parallelism: argon2.defaults.parallelism + 3
       })
-    }
-  }, {
-    name: 'argon2#hashArgon2d',
-    func: async () => {
+    },
+    'argon2#hashArgon2d': async () => {
       await argon2.hash(password, salt, {
         argon2d: true
       })
-    }
-  }, {
-    name: 'argon2#verify',
-    func: async () => await argon2.verify(hash, password)
-  }, {
-    name: 'argon2#generateSalt',
-    func: async () => await argon2.generateSalt()
-  }]
+    },
+    'argon2#verify': async () => await argon2.verify(hash, password),
+    'argon2#generateSalt': async () => await argon2.generateSalt()
+  }
 
-  for (const item of fixtures) {
-    benchmark(item.name, async () => {
-      await item.func()
-    }, (err, ev) => {
-      console.log(ev.target.toString())
-      console.log(err)
+  const suite = new Suite()
+  for (const item of Object.keys(fixtures)) {
+    suite.add(item, async () => {
+      await fixtures[item]
     })
   }
+  suite.on('cycle', event => {
+    console.log(event.target.toString())
+  }).run()
 })()
