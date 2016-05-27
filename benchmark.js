9069d4ac2c50dd99162465911c0e1a4409ed64c1
Ranieri Althoff <ranisalt@gmail.com>
2016-05-27 20:07:31 -0300

    Fix benchmarks to work with async/await

--- benchmark.js
+++ benchmark.js
@@ -7,40 +7,53 @@ import argon2 from './'
   const hash = await argon2.hash(password, salt)
 
   const fixtures = {
-    'argon2#hash': async () => {
+    'argon2#hash': async deferred => {
       await argon2.hash(password, salt)
+      deferred.resolve()
     },
-    'argon2#hashTimeCost': async () => {
+    'argon2#hashTimeCost': async deferred => {
       await argon2.hash(password, salt, {
         timeCost: argon2.defaults.timeCost + 3
       })
+      deferred.resolve()
     },
-    'argon2#hashMemoryCost': async () => {
+    'argon2#hashMemoryCost': async deferred => {
       await argon2.hash(password, salt, {
         memoryCost: argon2.defaults.memoryCost + 3
       })
+      deferred.resolve()
     },
-    'argon2#hashParallelism': async () => {
+    'argon2#hashParallelism': async deferred => {
       await argon2.hash(password, salt, {
         parallelism: argon2.defaults.parallelism + 3
       })
+      deferred.resolve()
     },
-    'argon2#hashArgon2d': async () => {
+    'argon2#hashArgon2d': async deferred => {
       await argon2.hash(password, salt, {
         argon2d: true
       })
+      deferred.resolve()
     },
-    'argon2#verify': async () => await argon2.verify(hash, password),
-    'argon2#generateSalt': async () => await argon2.generateSalt()
+    'argon2#verify': async deferred => {
+      await argon2.verify(hash, password)
+      deferred.resolve()
+    },
+    'argon2#generateSalt': async deferred => {
+      await argon2.generateSalt()
+      deferred.resolve()
+    }
   }
 
-  const suite = new Suite()
+  const suite = new Suite({
+    onCycle(event) {
+      console.log(event.target.toString())
+    }
+  })
+
   for (const item of Object.keys(fixtures)) {
-    suite.add(item, async () => {
-      await fixtures[item]
-    })
+    suite.add(item, fixtures[item], {defer: true})
   }
-  suite.on('cycle', event => {
-    console.log(event.target.toString())
-  }).run()
+
+  suite.run({async: true})
 })()
