de08f355cfa88ba74de58ce7b50722adbef8c67a
Ranieri Althoff <ranisalt@gmail.com>
2018-03-26 23:07:37 -0300

    Unfix syntax to appease GCC<5, fixes #117

--- src/argon2_node.h
+++ src/argon2_node.h
@@ -7,14 +7,18 @@
 namespace NodeArgon2 {
 
 struct Options {
+    // TODO: remove ctors and initializers when GCC<5 stops shipping on Ubuntu
+    Options() = default;
+    Options(Options&&) = default;
+
     std::string salt;
 
-    uint32_t hash_length;
-    uint32_t time_cost;
-    uint32_t memory_cost;
-    uint32_t parallelism;
+    uint32_t hash_length = {};
+    uint32_t time_cost = {};
+    uint32_t memory_cost = {};
+    uint32_t parallelism = {};
 
-    argon2_type type;
+    argon2_type type = {};
 };
 
 class HashWorker final: public Nan::AsyncWorker {
