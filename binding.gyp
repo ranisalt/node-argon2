ranisalt/node-argon2

commit 9386c96c60ee024c4be45114658ff6837683eae2

Fix potential undefined behavior through C++17 new std::string::data() overload (#318)

binding.gyp
@@ -2,10 +2,21 @@
   "target_defaults": {
     "include_dirs": ["argon2/include"],
     "target_conditions": [
+      ["OS == 'linux'", {
+        "cflags_cc": ["-std=c++17"],
+      }],
       ["OS == 'mac'", {
         "xcode_settings": {
+          "CLANG_CXX_LANGUAGE_STANDARD":"c++17",
           "CLANG_CXX_LIBRARY": "libc++",
           "MACOSX_DEPLOYMENT_TARGET": "10.7",
+        }
+      }],
+      ["OS == 'win'", {
+        "msvs_settings": {
+          "VCCLCompilerTool": {
+            "AdditionalOptions": ["-std:c++17"],
+          }
         }
       }]
     ],
@@ -71,13 +82,11 @@
     }, {
       "target_name": "action_after_build",
       "type": "none",
-      "dependencies": [ "<(module_name)" ],
-      "copies": [
-        {
-          "files": [ "<(PRODUCT_DIR)/<(module_name).node" ],
-          "destination": "<(module_path)"
-        }
-      ]
+      "dependencies": ["<(module_name)"],
+      "copies": [{
+        "files": ["<(PRODUCT_DIR)/<(module_name).node"],
+        "destination": "<(module_path)"
+      }]
     }
   ]
 }

src/argon2_node.cpp
@@ -43,19 +43,19 @@ struct Options {
     argon2_type type;
 };
 
-argon2_context make_context(uint8_t* buf, const ustring& plain, const ustring& salt, const Options& opts)
+argon2_context make_context(uint8_t* buf, ustring& plain, ustring& salt, Options& opts)
 {
     argon2_context ctx;
 
     ctx.out = buf;
     ctx.outlen = opts.hash_length;
-    ctx.pwd = const_cast<uint8_t*>(plain.data());
+    ctx.pwd = plain.data();
     ctx.pwdlen = plain.size();
-    ctx.salt = const_cast<uint8_t*>(salt.data());
-    ctx.saltlen = salt.length();
-    ctx.secret = opts.secret.empty() ? nullptr : const_cast<uint8_t*>(opts.secret.data());
+    ctx.salt = salt.data();
+    ctx.saltlen = salt.size();
+    ctx.secret = opts.secret.empty() ? nullptr : opts.secret.data();
     ctx.secretlen = opts.secret.size();
-    ctx.ad = opts.ad.empty() ? nullptr : const_cast<uint8_t*>(opts.ad.data());
+    ctx.ad = opts.ad.empty() ? nullptr : opts.ad.data();
     ctx.adlen = opts.ad.size();
     ctx.t_cost = opts.time_cost;
     ctx.m_cost = opts.memory_cost;
@@ -81,9 +81,9 @@ public:
 
     void Execute() override
     {
-        uint8_t* buf = new uint8_t[opts.hash_length];
+        auto buf = std::make_unique<uint8_t[]>(opts.hash_length);
 
-        auto ctx = make_context(buf, plain, salt, opts);
+        auto ctx = make_context(buf.get(), plain, salt, opts);
         int result = argon2_ctx(&ctx, opts.type);
 
         if (result != ARGON2_OK) {
@@ -91,12 +91,10 @@ public:
             SetError(argon2_error_message(result));
             /* LCOV_EXCL_STOP */
         } else {
-            hash.assign(buf, opts.hash_length);
+            hash.assign(buf.get(), opts.hash_length);
         }
 
-        std::fill_n(buf, opts.hash_length, 0);
-
-        delete[] buf;
+        std::fill_n(buf.get(), opts.hash_length, 0);
     }
 
     void OnOK() override

commit fcc048c28bd75fee0571a7bcc1790fa33f8ffef2

Enforce C++17 on FreeBSD
This fixes the build issue introduced in 9386c96c60ee.

binding.gyp
@@ -2,7 +2,7 @@
   "target_defaults": {
     "include_dirs": ["argon2/include"],
     "target_conditions": [
-      ["OS == 'linux'", {
+      ["OS == 'linux' or OS == 'freebsd'", {
         "cflags_cc": ["-std=c++17"],
       }],
       ["OS == 'mac'", {